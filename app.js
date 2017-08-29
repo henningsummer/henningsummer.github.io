73fb9de0fc5075a08f61ddeacab047957a57ca59
Henning_Summer <henningsummer3@gmail.com>
2017-08-29 05:58:26 -0300

    mandoando m

--- app.js
+++ app.js
@@ -2,33 +2,33 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var clients = {}; 
+var clients = {};
 
-app.get('/', function(req, res){
-  res.send('server is running');
+app.get('/', function(req, res) {
+    res.send('server is running now');
 });
 
-io.on("connection", function (client) {  
-    client.on("join", function(name){
-    	console.log("Joined: " + name);
+io.on("connection", function(client) {
+    client.on("join", function(name) {
+        console.log("Joined: " + name);
         clients[client.id] = name;
         client.emit("update", "You have connected to the server.");
         client.broadcast.emit("update", name + " has joined the server.")
     });
 
-    client.on("send", function(msg){
-    	console.log("Message: " + msg);
+    client.on("send", function(msg) {
+        console.log("Message: " + msg);
         client.broadcast.emit("chat", clients[client.id], msg);
     });
 
-    client.on("disconnect", function(){
-    	console.log("Disconnect");
+    client.on("disconnect", function() {
+        console.log("Disconnect");
         io.emit("update", clients[client.id] + " has left the server.");
         delete clients[client.id];
     });
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on port 3000');
+http.listen(3000, function() {
+    console.log('listening on port 3000');
 });
